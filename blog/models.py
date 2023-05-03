ae8bdf29ed1daeed235d68e023ee9c9ba9112698
Nikita <kyle.winner@gmail.com>
2023-05-03 14:48:20 +0300

    minimal static site with Django

---
+++ blog/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+
+class Post(models.Model):
+    """ Модель данных для постов блога. """
+    class Status(models.TextChoices):
+        """ Модель для статуса Черновик/Опубликован. """
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='blog_posts')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)  # время публикации поста
+    created = models.DateTimeField(auto_now_add=True)  # время создания поста
+    updated = models.DateTimeField(auto_now=True)  # время обновления поста
+    status = models.CharField(max_length=2,
+                              choices=Status.choices,
+                              default=Status.DRAFT)
+
+    objects = models.Manager() # менеджер, применяемый по умолчанию
+    published = PublishedManager() # конкретно-прикладной менеджер
+
+    class Meta:
+        ordering = ['-publish']  # убывающий хронологический порядок
+        indexes = [
+            models.Index(fields=['-publish']),
+        ]
+
+    def __str__(self):
+        return self.title
