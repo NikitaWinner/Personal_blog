695e1cd1b4747dd658bcd1d6e63debc7319bcc5c
Nikita <kyle.winner@gmail.com>
2023-05-09 15:56:21 +0300

    search engine optimization friendly urls

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
-from django.utils import timezone
 from django.urls import reverse
+from django.utils import timezone
 
 
 class PublishedManager(models.Manager):
@@ -19,8 +19,11 @@ class Post(models.Model):
         PUBLISHED = "PB", "Published"
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    slug = models.SlugField(max_length=250, 
+                            unique_for_date='publish')
+    author = models.ForeignKey(User, 
+                               on_delete=models.CASCADE, 
+                               related_name="blog_posts")
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)  # время публикации поста
     created = models.DateTimeField(auto_now_add=True)  # время создания поста
@@ -38,10 +41,13 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-    
+
     def get_absolute_url(self):
         """Функция reverse() будет формировать
-        URL-адрес динамически, применяя имя URL-адреса, 
+        URL-адрес динамически, применяя имя URL-адреса,
         определенное в  шаблонах URL-адресов."""
-        return reverse('blog:post_detail',
-                       args=[self.id])
+        return reverse("blog:post_detail", 
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
