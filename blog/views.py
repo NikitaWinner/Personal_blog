04660a401c554d73f7d35f9bb7a078ec13173022
Nikita <kyle.winner@gmail.com>
2023-05-12 00:37:16 +0300

    Sending emails

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,41 @@
-from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView
+
 from .models import Post
+from .forms import EmailPostForm
+from django.core.mail import send_mail
+
+def post_share(request, post_id):
+    # Извлечь пост по идентификатору id
+    post = get_object_or_404(Post,
+                             id=post_id,
+                             status=Post.Status.PUBLISHED)
+    sent = False
+
+    if request.method == 'POST':
+        # Форма была передана на обработку
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Поля формы успешно прошли валидацию
+            cd = form.cleaned_data
+            # ... отправить электронное письмо
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n {cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'melnov.nikita@gmail.com',[cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
 
 class PostListView(ListView):
-    """ Альтернативное представление списка постов """
+    """Альтернативное представление списка постов"""
 
     queryset = Post.published.all()
-    context_object_name = 'posts'
+    context_object_name = "posts"
     paginate_by = 3
-    template_name = 'blog/post/list.html'
+    template_name = "blog/post/list.html"
+
 
 # def post_list(request):
 #     post_list = Post.published.all()
--- mysite/settings.py
+++ mysite/settings.py
@@ -124,3 +124,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Конфигурация сервера электронной почты
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'melnov.nikita@gmail.com'
+EMAIL_HOST_PASSWORD = 'obztjwcrzscgddik'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
