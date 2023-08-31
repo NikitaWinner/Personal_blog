b637017b14953991738aea07decb955f0125700c
Nikita <kyle.winner@gmail.com>
2023-08-31 13:59:56 +0300

    full-text search

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,9 @@ from django.views.decorators.http import require_POST
 from django.views.generic import ListView
 from taggit.models import Tag
 
-from .forms import CommentForm, EmailPostForm
+from django.contrib.postgres.search import SearchVector, \
+    SearchQuery, SearchRank
+from .forms import CommentForm, EmailPostForm, SearchForm
 from .models import Post
 
 
@@ -124,3 +126,27 @@ def post_comment(request, post_id):
         "blog/post/comment.html",
         {"post": post, "form": form, "comment": comment},
     )
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            search_vector = SearchVector('title', config='spanish', weight='A') + \
+                            SearchVector('body', config='spanish', weight='B')
+            search_query = SearchQuery(query, config='spanish')
+            results = Post.published.annotate(search=search_vector,
+                                              rank=SearchRank(search_vector,
+                                                              search_query)).filter(search=search_query,
+                                                                                    rank__gte=0.3).order_by('-rank')
+    return render(request,
+                  'blog/post/search.html',
+                  {'form': form,
+                   'query': query,
+                   'results': results})
+
